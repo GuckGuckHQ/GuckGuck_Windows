d68c03043f9d715ddba0a57437b9feeb7dbb3c51
nor0x <nor0x@users.noreply.github.com>
2025-03-09 11:46:33 +0100

    validation and fixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,6 +16,9 @@ namespace GuckGuck;
 public partial class MainWindow : Window
 {
     private ScreenshotTimerService _screenshotTimerService;
+    private string _currentId;
+    private bool _isCapturing = false;
+
 
     public MainWindow()
     {
@@ -54,20 +58,45 @@ public partial class MainWindow : Window
 
     private async void ScreenshotButton_Click(object sender, RoutedEventArgs e)
     {
-        UpdateInputRect();
-        _screenshotTimerService.Start();
-        await _screenshotTimerService.CaptureAndUploadScreenshot("screenshot");
+        if (_isCapturing)
+        {
+            _screenshotTimerService.Stop();
+            StartButton.Content = "Start";
+        }
+        else
+        {
+            StartButton.Content = "Stop";
+            UpdateInputRect();
+            _screenshotTimerService.Start();
+            _currentId = Guid.NewGuid().ToString("N");
+            await _screenshotTimerService.CaptureAndUploadScreenshot(_currentId);
+        }
+        _isCapturing = !_isCapturing;
+
+
+        UrlTextBox.Text = $"http://localhost:5137/{_currentId}";
     }
 
-    private async void FullscreenCaptureButton_Click(object sender, RoutedEventArgs e)
-    {
-        _screenshotTimerService.Start();
-        var screenWidth = (int)SystemParameters.PrimaryScreenWidth;
-        var screenHeight = (int)SystemParameters.PrimaryScreenHeight;
-        var inputRect = new System.Drawing.Rectangle(0, 0, screenWidth, screenHeight);
+    //private async void FullscreenCaptureButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    _screenshotTimerService.Start();
+    //    var screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+    //    var screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+    //    var inputRect = new System.Drawing.Rectangle(0, 0, screenWidth, screenHeight);
 
-        _screenshotTimerService.InputRect = inputRect;
-        await _screenshotTimerService.CaptureAndUploadScreenshot("fullscreen_screenshot");
+    //    _screenshotTimerService.InputRect = inputRect;
+    //    await _screenshotTimerService.CaptureAndUploadScreenshot("fullscreen_screenshot");
+    //}
+
+    private void IntervalTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        e.Handled = !IsTextAllowed(e.Text);
+    }
+
+    private static bool IsTextAllowed(string text)
+    {
+        Regex regex = new Regex("[^0-9]+"); // Regex that matches disallowed text
+        return !regex.IsMatch(text);
     }
 
     private void IncreaseInterval_Click(object sender, RoutedEventArgs e)
@@ -75,8 +104,17 @@ public partial class MainWindow : Window
         if (int.TryParse(IntervalTextBox.Text, out int interval))
         {
             interval++;
-            IntervalTextBox.Text = interval.ToString();
         }
+        else
+        {
+            interval = 1;
+        }
+        if (interval == 0)
+        {
+            interval = 1;
+        }
+        IntervalTextBox.Text = interval.ToString();
+        _screenshotTimerService.UpdateInterval(ConvertToMilliseconds(interval));
     }
 
     private void DecreaseInterval_Click(object sender, RoutedEventArgs e)
@@ -84,7 +122,30 @@ public partial class MainWindow : Window
         if (int.TryParse(IntervalTextBox.Text, out int interval) && interval > 0)
         {
             interval--;
-            IntervalTextBox.Text = interval.ToString();
+        }
+        else
+        {
+            interval = 1;
+        }
+        if (interval == 0)
+        {
+            interval = 1;
+        }
+        IntervalTextBox.Text = interval.ToString();
+        _screenshotTimerService.UpdateInterval(ConvertToMilliseconds(interval));
+    }
+
+    private int ConvertToMilliseconds(int interval)
+    {
+        switch (UnitButton.Content.ToString())
+        {
+            case "Minutes":
+                return interval * 60 * 1000;
+            case "Hours":
+                return interval * 60 * 60 * 1000;
+            case "Seconds":
+            default:
+                return interval * 1000;
         }
     }
 
@@ -106,6 +167,30 @@ public partial class MainWindow : Window
             case "Hours":
                 UnitButton.Content = "Seconds";
                 break;
+        }
+    }
+
+    private void VisitButton_Click(object sender, RoutedEventArgs e)
+    {
+        var url = UrlTextBox.Text;
+        if (!string.IsNullOrEmpty(url))
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open URL: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("URL cannot be empty.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
     }
 }
--- ScreenshotTimerService.cs
+++ ScreenshotTimerService.cs
@@ -11,9 +11,10 @@ namespace GuckGuck;
 public class ScreenshotTimerService : IDisposable
 {
 	private readonly Timer _timer;
+	private string _currentId;
 	public Rectangle InputRect { get; set; }
 
-    public ScreenshotTimerService(double interval)
+	public ScreenshotTimerService(double interval)
 	{
 		_timer = new Timer(interval);
 		_timer.Elapsed += OnTimedEvent;
@@ -30,18 +31,29 @@ public class ScreenshotTimerService : IDisposable
 		_timer.Stop();
 	}
 
-
-	private async void OnTimedEvent(object sender, ElapsedEventArgs e)
+	public void UpdateInterval(double interval)
 	{
-		await CaptureAndUploadScreenshot("timed_screenshot");
+		_timer.Interval = interval;
 	}
 
-	public async Task CaptureAndUploadScreenshot(string fileNamePrefix)
+	public void UpdateCurrentId(string id)
+    {
+        _currentId = id;
+    }
+
+
+    private async void OnTimedEvent(object sender, ElapsedEventArgs e)
 	{
-		var screenshotService = new Screenshotter();
+		await CaptureAndUploadScreenshot(_currentId);
+	}
+
+	public async Task CaptureAndUploadScreenshot(string id)
+	{
+		UpdateCurrentId(id);
+        var screenshotService = new Screenshotter();
 		var bytes = screenshotService.Capture(InputRect);
 
-		using (var fs = File.OpenWrite(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileNamePrefix + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png")))
+		using (var fs = File.OpenWrite(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), id + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png")))
 		{
 			fs.Write(bytes, 0, bytes.Length);
 		}
@@ -49,9 +61,11 @@ public class ScreenshotTimerService : IDisposable
 		var client = new HttpClient();
 		var content = new MultipartFormDataContent();
 		content.Add(new ByteArrayContent(bytes), "Image", "screenshot.png");
-		content.Add(new StringContent("screenshot"), "Id");
+		content.Add(new StringContent(_currentId), "Id");
 		var response = await client.PostAsync("http://localhost:5137/image", content);
-        screenshotService.Dispose();
+		var url = await response.Content.ReadAsStringAsync();
+		Debug.WriteLine(url);
+		screenshotService.Dispose();
 	}
 
 	public void Dispose()
